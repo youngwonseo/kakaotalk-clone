11680de74b10f0b1f6f1f1ab71af9f72b71f7454
Doctor's Fab <jazz9008@gmail.com>
2021-01-12 20:38:28 +0900

    backend

--- back-end/src/dao/User.ts
+++ back-end/src/dao/User.ts
@@ -1,27 +1,44 @@
 import { Injectable, Inject} from '@nestjs/common';
 import User, { UserModel, UserInterface } from "../database/models/User";
+import { AddUserDto } from '../dto/users';
 // import { AddUserDto } from "../dto";
 
 
 
-// @Injectable()
+
+@Injectable()
 export class UserDao {
   constructor(@Inject("USERS") private user: typeof User){}
 
+
+  
   public getList(): Promise<[UserInterface]>{
     return this.user.find().exec();
   }
 
-  public getById(id: number): Promise<UserInterface> {
-    
+  public getOne(id: number): Promise<UserInterface> {
     //
     return this.user.findById(id).exec();
   }
 
+  public getOneWithFriends() {
 
-  public add(addUserDto: AddUserDto): Promise<User> {
+
+  }
+
+
+  public create(addUserDto: AddUserDto): Promise<UserInterface> {
     console.log(addUserDto);
     return this.user.create(addUserDto);
+  }
+
+  public update(addUserDto: AddUserDto): Promise<UserInterface> {
+    
+    return this.user.findByIdAndUpdate();
+  }
+
+  public delete(){
+
   }
 
   
--- back-end/src/database/models/Message.ts
+++ back-end/src/database/models/Message.ts
@@ -1,13 +1,44 @@
 import { Document, Model, model, Types, Schema, Query } from "mongoose"
 // import crypto from 'crypto';
 // import jwt from 'jsonwebtoken';
+import { UserInterface } from './User';
 
 // properties, methods
 export interface MessageInterface{
-  username: string;
-  statusMessage: string;
-  email: string;
-  hashedPassword: string;
-  config: any;
+  user: UserInterface;
+  contents: string;
+  count: number;
   createdAt: any;
 }
+
+
+
+// for mongodb schema
+interface MessageBaseDocument extends MessageInterface, Document {}
+
+
+// static
+interface MessageDocument extends MessageBaseDocument {
+  // findByUsername(name: string): any;  
+}
+
+
+export interface MessageModel extends Model<MessageDocument> {
+
+}
+
+
+const MessageSchema = new Schema<MessageDocument, MessageModel>({
+  user: {
+    type: String,
+    ref: 'User',
+  },
+  contents: String,
+  count: Number,
+  createdAt: { type: Date, default: Date.now },
+});
+
+
+export default model<MessageDocument, MessageModel>('Chat', MessageSchema);
+
+
